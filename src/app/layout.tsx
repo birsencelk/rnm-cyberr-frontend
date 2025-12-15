d26b1f789b5597c284f28739cca06b57a7030232
birsencelk <birsencelk@gmail.com>
2025-12-15 15:36:06 +0100

    feat: add Apollo Client setup and integrate ApolloClientProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import "./globals.css";
 
 import { HeroUIProvider } from "@heroui/react";
+import { ApolloClientProvider } from "./providers/ApolloClientProvider";
 
 export default function RootLayout({
   children,
@@ -10,8 +11,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Rick and Morty Cyberr Frontend</title>
+      <link
+        rel="icon"
+        href="https://app.cyberr.ai/my-favicon/web-app-manifest-192x192.png"
+        sizes="48x48"
+        type="image/x-icon"
+      ></link>
       <body>
-        <HeroUIProvider>{children}</HeroUIProvider>
+        <HeroUIProvider>
+          <ApolloClientProvider>{children}</ApolloClientProvider>
+        </HeroUIProvider>
       </body>
     </html>
   );
